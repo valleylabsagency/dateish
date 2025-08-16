062a9a19e96a0c4b085e3ebf88c4ea6acdf56a30
Tyler Valley <tylervalley@Tylers-MacBook-Pro-2.local>
2025-08-15 23:23:15 -0400

    Regenerate lockfile to fix npm ci on EAS

--- metro.config.js
+++ metro.config.js
@@ -1,7 +1,11 @@
-// Learn more https://docs.expo.io/guides/customizing-metro
-const { getDefaultConfig } = require('expo/metro-config');
+// metro.config.js
+const { getDefaultConfig } = require('@expo/metro-config');
 
-/** @type {import('expo/metro-config').MetroConfig} */
+/** @type {import('@expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
+
+// Keep this only if you truly need it for a package-exports issue.
+// Otherwise, delete the next line.
 config.resolver.unstable_enablePackageExports = false;
+
 module.exports = config;
